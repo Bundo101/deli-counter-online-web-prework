ee428e6feea1cdc5523f4166a6c6650232ab1508
James Britton <brittonjp@gmail.com>
2019-04-24 12:01:56 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,10 +4,7 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    queue = "The line is currently: "
-    katz_deli.each_with_index do |name, index|
-      queue << "#{index + 1}. #{name}"
-  end
-  puts queue
-  end
+    i = 0
+    while i < katz_deli.length
+      
 end
