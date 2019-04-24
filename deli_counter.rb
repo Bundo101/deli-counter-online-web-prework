db4dbc0098955649be70e806845ccb61b46aadfa
James Britton <brittonjp@gmail.com>
2019-04-24 11:57:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,6 +5,6 @@ def line(katz_deli)
     puts "The line is currently empty."
   else
     queue = "The line is currently: "
-    katz_deli.each_with_index do 
+    katz_deli.each_with_index do |name, index|
   end
 end
