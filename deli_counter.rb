a7c5f0ddb09c803e502bcf6f042bf81a0dbfe466
James Britton <brittonjp@gmail.com>
2019-04-24 11:45:11 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,13 @@
 katz_deli = []
 
-def line(deli)
-  if deli.length == 0
+def line(katz_deli)
+  if katz_deli.length == 0
     puts "The line is currently empty."
   else
     i = 0
     customers = []
-    while i < deli.length do 
-      customers.push ("#{i + 1}. #{deli[i]}")
+    while i < katz_deli.length do 
+      customers.push ("#{i + 1}. #{katz_deli[i]}")
       i += 1
     end
   end
