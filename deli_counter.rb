eeb43911344e5c7a166ceddf7726531efbf849ce
James Britton <brittonjp@gmail.com>
2019-04-24 11:48:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,15 @@
-def line(katz_deli)
-   if katz_deli.count == 0
-     puts "The line is currently empty."
-   else
-      line = "The line is currently:"
-    katz_deli.each_with_index do |name, index|
-    line << " #{index +1}. #{name}"
+katz_deli = []
 
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty."
+  else
+    i = 0
+    customers = []
+    while i < katz_deli.length do 
+      customers.push ("#{i + 1}. #{katz_deli[i]}")
+      i += 1
     end
-    puts line
-    end
+  end
+  puts "The line is currently: #{customers.join(' ')}"
 end
