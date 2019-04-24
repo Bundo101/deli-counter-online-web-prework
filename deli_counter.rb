5688858f267731e1113eeb43a8020cb4e3796932
James Britton <brittonjp@gmail.com>
2019-04-24 11:47:48 +0000

    Automatically backed up by Learn

---
+++ deli_counter.rb
@@ -0,0 +1,12 @@
+def line(katz_deli)
+   if katz_deli.count == 0
+     puts "The line is currently empty."
+   else
+      line = "The line is currently:"
+    katz_deli.each_with_index do |name, index|
+    line << " #{index +1}. #{name}"
+
+    end
+    puts line
+    end
+end
